b8dd2e64e7c3e2818f15822e838694a3d45dfb0a
Chenlinjia Yi <yichenlinjia@gmail.com>
2025-10-21 23:18:07 +0200

    maquina de estado base

---
+++ src/StateMachine.js
@@ -0,0 +1,49 @@
+ //Clase base maquina de estados que controla que estado esta activo.
+ 
+export default class StateMachine {
+    constructor(context, id) {
+        this.id = id;                 // identificador, la etiqueta
+        this.context = context;       // objeto que la usa, ej: player
+        this.states = new Map();      // todos los estados registrados
+        this.currentState = null;     // estado actual
+    }
+
+    addState(name, state) {
+        this.states.set(name, state);
+        state.stateMachine = this; 
+    }
+
+    setState(name) {
+        const newState = this.states.get(name);
+        if (!newState) {
+            console.warn('estado no encontrado');
+            return;
+        }
+
+        // llama exit del estado actual
+        if (this.currentState && this.currentState.exit) {
+            this.currentState.exit(this.context);
+        }
+
+        this.previousState = this.currentState;
+        this.currentState = newState;
+
+        // llamar enter del nuevo estado
+        if (this.currentState.enter) {
+            this.currentState.enter(this.context);
+        }
+    }
+
+    step(time, delta) {
+        if (this.currentState && this.currentState.execute) {
+            this.currentState.execute(this.context, time, delta);
+        }
+    }
+
+    getStateName() {
+        for (const [name, state] of this.states.entries()) {
+            if (state === this.currentState) return name;
+        }
+        return null;
+    }
+}
