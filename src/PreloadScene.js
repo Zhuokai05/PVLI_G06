862c5647e4c0d04131e210ff873c9a5267a3c99c
Zhuokai Zhu <zhuzhu@ucm.es>
2025-10-23 22:46:40 +0200

    Meter sprites de enemigos basicos como muestra

--- src/PreloadScene.js
+++ src/PreloadScene.js
@@ -4,10 +4,14 @@ export class PreloadScene extends Phaser.Scene
   {
     //imagenes  y sprites
     this.load.image('player', './assets/Player/Angel.png');
-    this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
+    this.load.image('ground', './assets/Escena/Plataforma ira.png');
     this.load.image('background', './assets/Menu/logo.png.png');
     this.load.image('jugar', './assets/Menu/jugar.png');
     this.load.image('name', './assets/Menu/name.png');
+    this.load.image('basicEnemyAngry', './assets/Enemigos basicos/Fire.png');
+    this.load.image('basicEnemySad', './assets/Enemigos basicos/Water.png');
+    this.load.image('basicEnemyHappy', './assets/Enemigos basicos/Sun.png');
+    this.load.image('basicEnemyFear', './assets/Enemigos basicos/Ghost.png');
   }
 
   create() 
--- src/testPlayerScene.js
+++ src/testPlayerScene.js
@@ -10,10 +10,17 @@ export default class TestPlayerScene extends Phaser.Scene {
         this.inputManager = new InputManager(this);
 
         const ground = this.physics.add.staticGroup();
-        ground.create(192, 220, 'ground').setScale(2).refreshBody();
+        ground.create(300, 220, 'ground').setScale(2).refreshBody();
+        ground.create(900, 600, 'ground').setScale(2).refreshBody();
+        ground.create(220, 750, 'ground').setScale(2).refreshBody();
 
         this.player = new Player(this, 100, 100);
         this.physics.add.collider(this.player, ground);
+
+        this.basicEnemyAngry = this.add.sprite(300, 100, 'basicEnemyAngry');
+        this.basicEnemySad = this.add.sprite(400, 100, 'basicEnemySad');
+        this.basicEnemyHappy = this.add.sprite(900, 480, 'basicEnemyHappy');
+        this.basicEnemyFear = this.add.sprite(100, 650, 'basicEnemyFear');
     }
 
     update(time, delta) {
