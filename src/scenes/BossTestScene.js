8224fcc208959ecf3771f1755a1280c34694b75b
JaviCace <Javicace@ucm.es>
2025-11-07 18:01:45 +0100

    push

--- src/scenes/BossTestScene.js
+++ src/scenes/BossTestScene.js
@@ -42,7 +42,7 @@ class BossTestScene extends Phaser.Scene {
         // Configurar tecla ESC para pausa
         this.input.keyboard.on('keydown-ESC', () => {
             this.scene.pause('BossScene'); 
-            this.scene.launch('Pause');    
+            this.scene.launch('Pause' , { file: 'BossScene' });    
         });
     }
 
