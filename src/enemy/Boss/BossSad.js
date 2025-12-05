56adcc4c1117063f7bcc5892b2b980850b615442
Zhuokai Zhu <zhuzhu@ucm.es>
2025-12-05 13:02:57 +0100

    Añadir prueba de boss tristeza en bosstestscene

--- src/enemy/Boss/BossSad.js
+++ src/enemy/Boss/BossSad.js
@@ -194,15 +194,6 @@ export default class BossSad extends Phaser.Physics.Arcade.Sprite {
     destroyWaterBall(waterBall) {
         if (waterBall && waterBall.active) {
             waterBall.destroy();
-
-            const explosion = this.scene.add.circle(
-                waterBall.x, 
-                waterBall.y, 
-                20, 
-                0x00ffff, 
-                0.5
-            );
-            this.scene.time.delayedCall(200, () => explosion.destroy());
         }
     }
 
