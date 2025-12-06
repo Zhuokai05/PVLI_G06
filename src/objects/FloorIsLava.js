5c3132899e4149be0dd9b055a41935880a5c4b70
Bucerillo <153779274+Bucerillo@users.noreply.github.com>
2025-12-06 14:22:09 +0100

    Lava arreglado y listo para boss final

--- src/objects/FloorIsLava.js
+++ src/objects/FloorIsLava.js
@@ -50,6 +50,7 @@ export default class FloorIsLava extends Phaser.GameObjects.TileSprite {
 
     // Iniciar la subida de la lava
     startLava() {
+        this.y = this.playerRef.y + 300;
         this.isRising = true;
         this.setVisible(true);
     }
