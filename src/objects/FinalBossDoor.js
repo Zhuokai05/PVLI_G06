eea230a9a94ae58dec2d3b5bfa83f411ff63f8b4
Chenlinjia Yi <yichenlinjia@gmail.com>
2025-12-10 04:57:42 +0100

    Arreglar puerta boss final

--- src/objects/FinalBossDoor.js
+++ src/objects/FinalBossDoor.js
@@ -1,11 +1,14 @@
 import DoorBoss from '../objects/BossDoor.js';
+import PlayerDataManager from '../managers/PlayerDataManager.js';
 
 export default class FinalBossDoor extends DoorBoss {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
 
- this.iraboss = false;
- this.tristeboss = false;
+        if (PlayerDataManager.data.bossStatus.anger) this.iraboss = PlayerDataManager.data.bossStatus.anger;
+        else this.iraboss = false;
+        if (PlayerDataManager.data.bossStatus.sadness) this.tristeboss = PlayerDataManager.data.bossStatus.sadness;
+        else this.tristeboss = false;
     }
 
     activarIra() {
@@ -16,10 +19,9 @@ export default class FinalBossDoor extends DoorBoss {
         this.tristeboss = true;
     }
 
-   
+
     abrirPuerta() {
-        if (!this.tristeboss|| !this.iraboss) 
-        {
+        if (!this.tristeboss || !this.iraboss) {
             console.log("No se puede abrir la puerta: faltan botones");
             return;
         }
